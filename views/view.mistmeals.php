becd3edbf75f392acbe4e7563963094fa1a9311c
Kyubiful <ser.zabala@gmail.com>
2022-02-12 00:25:32 +0100

    design: added nutrients form

--- views/view.mistmeals.php
+++ views/view.mistmeals.php
@@ -81,20 +81,71 @@
 
 <!-- Modal -->
 <?php
+
   foreach($product_variations_list as $key => $product) {
+   
     $product_variations_id = $product['ID'];
+    $product_name = $product['post_title'];
+    $table = $prefix . 'mm_nutrientes';
+    $query = "SELECT * FROM $table WHERE id LIKE $product_variations_id";
+    $product = $wpdb->get_results($query, ARRAY_A);
+
     echo "
     <div class='modal modal-$product_variations_id fade' id='addNutrientsModal-$product_variations_id' tabindex='-1' role='dialog' aria-labelledby='exampleModalCenterTitle' aria-hidden='true'>
       <div class='modal-dialog modal-dialog-centered' role='document'>
         <div class='modal-content'>
           <div class='modal-header'>
             <h5 class='modal-title' id='exampleModalLongTitle'>Nutrientes</h5>
-            <button type='button' class='close' id='modal-close-$product_variations_id' aria-label='Close'>
-              <span aria-hidden='true'>&times;</span>
+            <button type='button' class='btn-close' id='modal-close-$product_variations_id' aria-label='Close'>
             </button>
           </div>
           <div class='modal-body'>
-            ...
+            <p>$product_name</p>
+            <table>
+            <div class='row'>
+              <div class='form-group col-6'> 
+                <label for='energia'>Energía</label>
+                <input class='form-control' type='number' name='energia' placeholder='Energía' value=''/>
+              </div> 
+              <div class='form-group col-6'> 
+                <label for='energia'>Calorías</label>
+                <input class='form-control' type='number' name='calorias' placeholder='Calorías' value=''/>
+              </div> 
+            </div> 
+            <div class='row'>
+              <div class='form-group col-6'> 
+                <label for='energia'>Proteínas</label>
+                <input class='form-control' type='number' name='proteinas' placeholder='Proteínas' value=''/>
+              </div> 
+              <div class='form-group col-6'> 
+                <label for='energia'>Grasas</label>
+                <input class='form-control' type='number' name='grasas' placeholder='Grasas' value=''/>
+              </div> 
+            </div> 
+            <div class='row'>
+              <div class='form-group col-6'> 
+                <label for='energia'>Saturadas</label>
+                <input class='form-control' type='number' name='saturadas' placeholder='Saturadas' value=''/>
+              </div> 
+              <div class='form-group col-6'> 
+                <label for='energia'>Carbohidratos</label>
+                <input class='form-control' type='number' name='carbohidratos' placeholder='Carbohidratos' value=''/>
+              </div> 
+            </div> 
+            <div class='row'>
+              <div class='form-group col-6'> 
+                <label for='energia'>Azucar</label>
+                <input class='form-control' type='number' name='azucar' placeholder='Azucar' value=''/>
+              </div> 
+              <div class='form-group col-6'> 
+                <label for='energia'>Fibra</label>
+                <input class='form-control' type='number' name='fibra' placeholder='Fibra' value=''/>
+              </div> 
+            </div> 
+            <div class='d-flex justify-content-end mt-2'>
+              <button class='btn btn-primary' type='submit' value='$product_variations_id'>Guardar</button>
+            </div>
+            </table>
           </div>
         </div>
       </div>
