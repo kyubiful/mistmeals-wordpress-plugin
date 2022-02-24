f4ef823969f8d59c5c9a4895488faaea5f804867
Kyubiful <serzmr@gmail.com>
2022-02-24 17:26:24 +0100

    feat: add ajax php to loaded moduls

--- load_mistmeals.php
+++ load_mistmeals.php
@@ -2,3 +2,4 @@
 
 require_once MISTMEALS_PLUGIN_DIR . 'class.mistmeals.php';
 require_once MISTMEALS_PLUGIN_DIR . 'class.mistmeals_menu.php';
+require_once MISTMEALS_PLUGIN_DIR . 'ajax.mistmeals.php';
